b6c1389610fa7c3cd50090796dd9affe95e06c3b
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-04-12 14:08:45 +0530

    JAVA

---
+++ CF_1950A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CF_1950A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for(int i=0;i<t;i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+        if(a<b && b<c){
+            System.out.println("STAIR");
+        }
+        else if (a<b && b>c) {
+            System.out.println("PEAK");
+        }
+        else{
+            System.out.println("NONE");
+        }
+        }
+    }
+}
