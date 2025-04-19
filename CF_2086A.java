eebe465962a51d541dca57e7ecf3ca3fd9978295
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-04-19 23:25:32 +0530

    JAVA

---
+++ CF_2086A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CF_2086A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t=scanner.nextInt();
+        for (int i=0; i<t;i++) {
+            int n =scanner.nextInt();  
+            System.out.println(2*n);  
+        }
+    }
+}
