792a31f533731a03e0edb4b39c48875e8043a3fa
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-04-20 17:25:20 +0530

    Add files via upload

---
+++ CF_2020A.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CF_2020A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            long n=scanner.nextInt();
+            long k=scanner.nextInt();
+            if (k == 1 || k > n) {
+                System.out.println(n);
+                continue;
+            }
+            int ans = 0;
+            while (n > 0) {
+                if (n % k != 0) {
+                    ans += n % k;
+                }
+                n /= k;
+            }
+    
+            System.out.println(ans);
+        }
+    }
+}
